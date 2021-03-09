070a3181a24ded3be148600d985c4c695a6f0bcc
yuvarajcbe <yuvaraj.cbe@gmail.com>
2021-03-09 00:09:53 -0500

    updated lockfile

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   backend "gcs" {
     bucket      = "yuva-global-gsb"
     prefix      = "network-tfsate"
-   # credentials = ".././GCP/calcium-field-306715-5333e7bec98a.json"
+   #credentials = ".././GCP/calcium-field-306715-5333e7bec98a.json"
   }
 }
 
